a0c81dad6d148de17d07ee52a0f96ad3fa79a49a
Saleh-Binessa <saleh.binessa@gmail.com>
2024-01-18 14:56:36 +0300

    ticket 5 done

--- app/src/main/java/com/example/miniprojectandroid/Repo/TransactionRepo.java
+++ app/src/main/java/com/example/miniprojectandroid/Repo/TransactionRepo.java
@@ -3,6 +3,8 @@ package com.example.miniprojectandroid.Repo;
 import com.example.miniprojectandroid.transaction_classes.Transaction;
 import com.example.miniprojectandroid.transaction_classes.TransactionType;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class TransactionRepo {
@@ -21,7 +23,16 @@ public class TransactionRepo {
 
     public ArrayList<Transaction> dummyData(int numofItem){
         ArrayList<Transaction> transactions = new ArrayList<>();
-        transactions.add(new Transaction(1,0001,"January 15,2024",100.00,1250.00, TransactionType.DEPOSITE));
+        String[] dates = {"January 19,2024", "January 14,2024", "January 15,2024", "January 16,2024", "January 17,2024", "January 18,2024"};
+        for(int i = numofItem; i < 8; i++)
+        {
+            int id = 1, accountNumber = 1001;
+            double amount = 200, balance = 1200;
+            double updatedAmount = amount * i;
+            double updatedBalance = (balance + updatedAmount);
+
+        transactions.add(new Transaction(id+i,accountNumber+i,dates[i%6],amount*i,updatedBalance, TransactionType.DEPOSITE));
+}
         return transactions;
     }
 }
